72cda2dfce75e3b3c254406a8b52a836a34e8673
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-23 23:19:30 +0545

    add darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { Button } from "./components/ui/button"
+import { ThemeProvider } from "./components/context/theme-provider"
+import { ModeToggle } from "./components/mode-toggle"
 
 function App() {
-  return(
-  <div className="h-screen w-screen flex flex-col gap-5 justify-center items-center bg-black text-white text-4xl"> 
-    <h1>Vite + React + Ts + Tailwind</h1>
-    <Button>Button</Button>
-  </div>
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="h-screen w-screen flex flex-col gap-5 justify-center items-center text-4xl ">
+        <h1>Vite + React + Ts + Tailwind</h1>
+        <p>Color Mode: <span className="hidden dark:inline">Dark</span> <span className="dark:hidden">Light</span></p>
+        <ModeToggle/>
+      </div>
+    </ThemeProvider>
   )
 }
 
