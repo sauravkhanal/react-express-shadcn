cd1d2948836333cdb313dd7404b3a5464c4c4be0
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-23 22:13:22 +0545

    vite + tailwind config

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return(
+  <div className="h-screen w-screen flex justify-center items-center bg-black text-white text-4xl"> 
+    <h1>Vite + React + Ts + Tailwind</h1>
+  </div>
+  )
+}
+
+export default App
