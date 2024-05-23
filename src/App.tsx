3d6010ef952890622f40bc932ea1b249afd44815
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-23 22:22:44 +0545

    shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return(
-  <div className="h-screen w-screen flex justify-center items-center bg-black text-white text-4xl"> 
+  <div className="h-screen w-screen flex flex-col gap-5 justify-center items-center bg-black text-white text-4xl"> 
     <h1>Vite + React + Ts + Tailwind</h1>
+    <Button>Button</Button>
   </div>
   )
 }
